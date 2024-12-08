44f6120b1161a3d7bd861ef33c1b42f5e404211b
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-12-09 00:11:35 +0200

    "created models Crew, Airplane, AirplaneType, Flight, Route, Airport, Ticket, Order"

--- user/models.py
+++ user/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=150)
