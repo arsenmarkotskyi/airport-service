e9f17d27daa34ece147f456329e6c2a73ff71adb
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-12-09 00:14:19 +0200

    "add Crew, Airplane, AirplaneType, Flight(with detail), Route, Airport, Ticket, Order to admin panel"

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Crew, Airplane,
+    AirplaneType, Flight,
+    Route, Airport,
+    Ticket, Order
+)
+
+admin.site.register(Crew)
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Flight)
+admin.site.register(Route)
+admin.site.register(Airport)
+admin.site.register(Ticket)
+admin.site.register(Order)
