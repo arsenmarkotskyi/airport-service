d4c0a71245f42092f5c1bfb665436cb80afcb944
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-12-09 00:13:58 +0200

    "created urls for Crew, Airplane, AirplaneType, Flight(with detail), Route, Airport, Ticket, Order"

---
+++ airport/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import CrewViewSet, AirplaneViewSet, AirplaneTypeViewSet, FlightViewSet, RouteViewSet, \
+    AirportViewSet, TicketViewSet, OrderViewSet
+
+app_name = 'airport'
+
+router = routers.DefaultRouter()
+router.register("crews", CrewViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("flights", FlightViewSet)
+router.register("routes", RouteViewSet)
+router.register("airports", AirportViewSet)
+router.register("tickets", TicketViewSet)
+router.register("orders", OrderViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
