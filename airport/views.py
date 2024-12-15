3f21e413eb99f5e8c4278622f3d43537fd2bc241
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-12-15 22:01:48 +0200

    "add extend_schema to RouteViewSet and FlightViewSet"

--- airport/views.py
+++ airport/views.py
@@ -1,5 +1,9 @@
 from django.db.models import Count, ExpressionWrapper, F, IntegerField, Q
+from django.utils.dateparse import parse_date
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 
 
@@ -7,6 +11,7 @@ from airport.models import Crew, Airplane, AirplaneType, Flight, Route, Airport,
 from airport.serializers import CrewSerializer, AirplaneSerializer, AirplaneTypeSerializer, FlightSerializer, \
     RouteSerializer, AirportSerializer, TicketSerializer, OrderSerializer, AirplaneListSerializer, RouteListSerializer, \
     FlightListSerializer, OrderListSerializer, TicketListSerializer, FlightDetailSerializer
+from .permission import IsAdminOrIfAuthenticatedReadOnly
 
 
 class CrewViewSet(mixins.CreateModelMixin,
@@ -15,6 +20,7 @@ class CrewViewSet(mixins.CreateModelMixin,
     GenericViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AirplaneViewSet(mixins.CreateModelMixin,
@@ -22,6 +28,7 @@ class AirplaneViewSet(mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
     GenericViewSet):
     queryset = Airplane.objects.all().select_related("airplane_type")
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
@@ -36,6 +43,7 @@ class AirplaneTypeViewSet(mixins.CreateModelMixin,
     GenericViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class FlightViewSet(mixins.CreateModelMixin,
@@ -45,6 +53,7 @@ class FlightViewSet(mixins.CreateModelMixin,
     queryset = Flight.objects.all().select_related(
         'route__source', 'route__destination', 'airplane'
     )
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
     def get_serializer_class(self):
@@ -54,12 +63,37 @@ class FlightViewSet(mixins.CreateModelMixin,
             return FlightDetailSerializer
         return FlightSerializer
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "departure_time",
+                type={"type": "string", "format": "date-time"},
+                description="Filter flight by departure_time "
+                            "Use ISO 8601 format "
+                            "(e.g., '2024-10-08').",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        departure_time = request.query_params.get("departure_time")
+        if departure_time:
+            date = parse_date(departure_time)
+            if date:
+                queryset = queryset.filter(departure_time__date=date)
+
+        self.queryset = queryset
+        return super().list(request, *args, **kwargs)
+
 
 class RouteViewSet(mixins.CreateModelMixin,
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     GenericViewSet):
     queryset = Route.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         queryset = self.queryset
@@ -85,6 +119,38 @@ class RouteViewSet(mixins.CreateModelMixin,
             return RouteListSerializer
         return RouteSerializer
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "route_from",
+                type=str,
+                description="Filter flight by town you route from "
+                            "Use format like Zolochiv",
+                required=False,
+            ),
+            OpenApiParameter(
+                "destination",
+                type=str,
+                description="Filter flight by destination "
+                            "Use format like Lviv",
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        route_from = request.query_params.get("route_from")
+        if route_from:
+            queryset = queryset.filter(source__name__icontains=route_from)
+
+        destination = request.query_params.get("destination")
+        if destination:
+            queryset = queryset.filter(destination__closet_big_city__icontains=destination)
+
+        self.queryset = queryset
+        return super().list(request, *args, **kwargs)
+
 
 class AirportViewSet(mixins.CreateModelMixin,
     mixins.ListModelMixin,
@@ -92,18 +158,12 @@ class AirportViewSet(mixins.CreateModelMixin,
     GenericViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-# class TicketViewSet(mixins.CreateModelMixin,
-#     mixins.ListModelMixin,
-#     mixins.RetrieveModelMixin,
-#     GenericViewSet):
-#     queryset = Ticket.objects.all()
-#
-#     def get_serializer_class(self):
-#         if self.action in ('list', 'retrieve'):
-#             return TicketListSerializer
-#         return TicketSerializer
+class OrderPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
 
 
 class OrderViewSet(mixins.CreateModelMixin,
@@ -117,6 +177,8 @@ class OrderViewSet(mixins.CreateModelMixin,
         'tickets__flight__route__source__airport',
         'tickets__flight__route__destination__airport'
     )
+    pagination_class = OrderPagination
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
